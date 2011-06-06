76e16fb5eb13da81af9e13663a0fa190f60af907
Eli Van Zoeren <eli@elivz.com>
2011-06-06 10:48:55 -0700

    Initial commit of working code

---
+++ language/english/lang.vz_bad_behavior.php
@@ -0,0 +1,16 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang = array(
+
+    // Settings labels
+	'strict' => 'Strict Mode',
+	'offsite_forms' => 'Allow off-site posting to forms',
+	'httpbl_key' => 'http:BL API Key',
+	'httpbl_threat' => 'http:BL Threat Level',
+	'httpbl_maxage' => 'http:BL Maximum Age',
+    
+	'' => ''
+);
+
+/* End of file lang.vz_bad_behavior.php */
+/* Location: /system/expressionengine/third_party/vz_bad_behavior/language/english/lang.vz_bad_behavior.php */
