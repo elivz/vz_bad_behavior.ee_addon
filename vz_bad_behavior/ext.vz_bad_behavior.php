693ef99dc5b837988e952739c987a9e63e027c77
Eli Van Zoeren <eli@elivz.com>
2012-01-03 22:39:34 -0800

    Re-instate blocked access logging.
    Bad Behavior dropped built-in DB logging at some point, so I had to
    explicitly include it in the extension.

--- vz_bad_behavior/ext.vz_bad_behavior.php
+++ vz_bad_behavior/ext.vz_bad_behavior.php
@@ -18,7 +18,7 @@ class Vz_bad_behavior_ext {
 	public $docs_url		= 'http://elivz.com/blog/single/bad_behavior/';
 	public $name			= 'VZ Bad Behavior';
 	public $settings_exist	= 'y';
-	public $version			= '1.1';
+	public $version			= '1.1.1';
 	
 	private $EE;
 	
@@ -209,6 +209,30 @@ function bb2_db_rows($result)
     {
         return $results->result_array() ;
     }
+}
+
+// Create the SQL query for inserting a record in the database.
+function bb2_insert($settings, $package, $key)
+{
+	$ip = bb2_db_escape($package['ip']);
+	$date = bb2_db_date();
+	$request_method = bb2_db_escape($package['request_method']);
+	$request_uri = bb2_db_escape($package['request_uri']);
+	$server_protocol = bb2_db_escape($package['server_protocol']);
+	$user_agent = bb2_db_escape($package['user_agent']);
+	$headers = "$request_method $request_uri $server_protocol\n";
+	foreach ($package['headers'] as $h => $v) {
+		$headers .= bb2_db_escape("$h: $v\n");
+	}
+	$request_entity = "";
+	if (!strcasecmp($request_method, "POST")) {
+		foreach ($package['request_entity'] as $h => $v) {
+			$request_entity .= bb2_db_escape("$h: $v\n");
+		}
+	}
+	return "INSERT INTO `" . bb2_db_escape($settings['log_table']) . "`
+		(`ip`, `date`, `request_method`, `request_uri`, `server_protocol`, `http_headers`, `user_agent`, `request_entity`, `key`) VALUES
+		('$ip', '$date', '$request_method', '$request_uri', '$server_protocol', '$headers', '$user_agent', '$request_entity', '$key')";
 }
 
 // Return emergency contact email address.
