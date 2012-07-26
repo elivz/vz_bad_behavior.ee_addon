28a695bf06bdb207756f45d6ac6ec29d32930484
Eli Van Zoeren <eli@elivz.com>
2012-07-26 15:03:31 -0700

    Update Bad Behavior to v2.2.7

--- vz_bad_behavior/bad-behavior/screener.inc.php
+++ vz_bad_behavior/bad-behavior/screener.inc.php
@@ -4,14 +4,10 @@
 
 function bb2_screener_cookie($settings, $package, $cookie_name, $cookie_value)
 {
-	// CHANGED: Use EE's set_cookie method, so the cookie consent module works
-	$EE =& get_instance();
-	$EE->functions->set_cookie($cookie_name, $cookie_value, 0);
-/*
+	// FIXME: Set the real cookie
 	if (!$settings['eu_cookie']) {
 		setcookie($cookie_name, $cookie_value, 0, bb2_relative_path());
 	}
-*/
 }
 
 function bb2_screener_javascript($settings, $package, $cookie_name, $cookie_value)
