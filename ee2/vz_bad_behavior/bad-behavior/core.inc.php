ef7557345999dc6d0e1a13c7721fc44a76c0c8a6
Eli Van Zoeren <eli@elivz.com>
2014-11-25 09:22:37 -0800

    Upgrade BB to v2.2.16

--- ee2/vz_bad_behavior/bad-behavior/core.inc.php
+++ ee2/vz_bad_behavior/bad-behavior/core.inc.php
@@ -1,5 +1,5 @@
 <?php if (!defined('BB2_CWD')) die("I said no cheating!");
-define('BB2_VERSION', "2.2.14");
+define('BB2_VERSION', "2.2.16");
 
 // Bad Behavior entry point is bb2_start()
 // If you're reading this, you are probably lost.
@@ -48,7 +48,7 @@ function bb2_reverse_proxy($settings, $headers_mixed)
 	if (!array_key_exists($header, $headers_mixed)) {
 		return false;
 	}
-	
+
 	$addrs = @array_reverse(preg_split("/[\s,]+/", $headers_mixed[$header]));
 	# Skip our known reverse proxies and private addresses
 	if (!empty($settings['reverse_proxy_addresses'])) {
