67dd5ff54f69843e59991b4ca38dd4f957c4aba8
ilab crossmedia og <martin@iMCJ27.local>
2016-04-26 18:16:52 +0200

    EE3 Support Added
    Changed the EE-Addon to support Expression Engine version 3.x.

---
+++ ee3/ee3_bad_behavior/views/logs.php
@@ -0,0 +1,29 @@
+<table class="mainTable padTable col-sortable" cellpadding="0" cellspacing="0" border="0">
+	<thead>
+		<th scope="col" class="headerSortUp"><?= lang('date') ?></th>
+		<th scope="col"><?= lang('ip') ?></th>
+		<th scope="col"><?= lang('uri') ?></th>
+		<th scope="col"><?= lang('method') ?></th>
+		<th scope="col"><?= lang('protocol') ?></th>
+		<th scope="col"><?= lang('user_agent') ?></th>
+		<th scope="col"><?= lang('key') ?></th>
+	</thead>
+
+	<tbody>
+	<?php foreach ($blocked as $i => $request) : ?>
+		<tr<?php if ($i % 2 == 0) echo ' class="odd"' ?>>
+			<td><?= $request['date'] ?></td>
+			<td><?= $request['ip'] ?></td>
+			<td style="overflow:hidden;max-width:350px" title"<?= $request['request_uri'] ?>"><?= $request['request_uri'] ?></td>
+			<td><?= $request['request_method'] ?></td>
+			<td><?= $request['server_protocol'] ?></td>
+			<td><?= $request['user_agent'] ?></td>
+			<td><?= '<a href="http://ioerror.us/bb2-support-key?key='.$request['key'].'" target="_blank">'.$request['key'].'</a>' ?></td>
+		</tr>
+	<?php endforeach; ?>
+	</tbody>
+</table>
+
+<?php
+/* End of file index.php */
+/* Location: ./system/user/addons/ee3_bad_behavior/views/logs.php */
